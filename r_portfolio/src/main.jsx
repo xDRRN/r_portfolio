3336405f04f92ab1986b5138fd3f178770279ad7
Darren Delgado <darren.delgado@etu.unistra.fr>
2025-01-30 10:30:26 +0100

    Initial changes of tutorial + css variables

--- r_portfolio/src/main.jsx
+++ r_portfolio/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import "@fontsource/outfit"
+import "@fontsource/roboto"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
